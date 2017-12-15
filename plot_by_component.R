09314692b441f652e24a484b40bb44daa9eecf84
brentpm2 <brentpm2@illinois.edu>
2017-12-15 11:15:34 -0600

    Add function, describe function with comments

---
+++ plot_by_component.R
@@ -0,0 +1,18 @@
+
+#constructs a bar chart of yield output all treatments containing a specific component
+#works within a specified trial, by default the first trial listed in the dataset will be used
+plot_by_component <- function(datafile,components,trial = "",variable = "DATA") {
+  #check to see if inputs are valid
+  
+  #get a list of treatments containing a component
+  
+  #get a list of treatments which possess the specified component
+  
+  #subset datafile by the list of treatments
+  
+  #graph the subset by the specified variable
+  
+  #export graph as pdf
+  
+  #export component list of all treatments used in graph to same pdf
+}
