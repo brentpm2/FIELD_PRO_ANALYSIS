79fd898ef4dadbe4f2fbc6e25676860c2a99c05a
brentpm2 <brentpm2@illinois.edu>
2017-12-15 09:48:46 -0600

    Added function output_standardized, added comment structure.

---
+++ output_standardized.R
@@ -0,0 +1,23 @@
+#for the specified columns within the inputted dataframe:
+#make the mean 0 and deviation 1
+#each column must only contain numeric data
+output_standardized <- function(datafile,col_names) {
+  #check if datafile is a dataframe or list
+  
+  #check if columns in col_names are all present within datafile
+  
+  #check if the columns in col_name are numeric
+  
+  
+  #get mean of each column, put into list
+  
+  #get st.dev of each column, put into list
+  
+  #subtract mean from each entry of the corresponding column, divide by st.dev
+  
+  #export as tab separated table
+  
+  #export mean and st.dev lists into tab separated file.
+  
+  #return modified dataset
+}
